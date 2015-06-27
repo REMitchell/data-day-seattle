e6816e3fafd1650efe49e6fe69df96bc3e7a0e7d
Ryan Mitchell <ryan@Ryans-MacBook-Air.local>
2015-06-27 10:56:52 -0700

    Removed definitions from topics

--- articleFinder/topic.py
+++ articleFinder/topic.py
@@ -6,8 +6,4 @@ class Topic:
       self.id = id
       self.name = name
 
-   def __init__(self, id, name, description):
-      self.id = id
-      self.name = name
-      self.description = description
    
