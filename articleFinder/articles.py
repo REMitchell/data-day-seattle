32f63aa0e2312dc6f6129ae72a4787875e7658ef
Ryan Mitchell <ryan@Ryans-MacBook-Air.local>
2015-06-27 06:37:37 -0700

    added comments

--- articleFinder/articles.py
+++ articleFinder/articles.py
@@ -1,6 +1,7 @@
 from website import Website
 from topic import Topic
 from content import Content
+
 import pymysql
 import requests
 from bs4 import BeautifulSoup
@@ -17,7 +18,10 @@ class Scraper:
 		global curcomp
 		
 
-
+	#########
+	# Open a MySQL connection. Should be triggered by the caller before running
+	# the scraper, if the caller is using MySQL
+	#########
 	def openCon(self):
 		global conn
 		global cur
@@ -29,15 +33,23 @@ class Scraper:
 		#cur = conn.cursor(pymysql.cursors.DictCursor)
 		#cur.execute("USE press")
 
+	#########
+	# Close a MySQL connection. Should be triggered by the caller after running
+	# the scraper, if the caller is using MySQL
+	#########
 	def closeCon(self):
 		global conn
 		global cur
 		#conn.close()
 
-	#Stores content if content does not already exist for that URL and topic
+	#########
+	# Prints and stores content if content does not already exist for that URL and topic
+	#########
 	def storeContent(self, topic, title, body, url):
 		global conn
 		global cur
+		#Optionally, comment out the print statements if you want this to go straight to
+		#MySQL without printing
 		print("New article found for: "+topic.name)
 		print(title)
 		print(body)
@@ -57,7 +69,10 @@ class Scraper:
 #			except:
 #				conn.rollback()
 
-	#Creates a new topic in the database, if one does not exist. Returns a topic object
+	#########
+	# Creates a new topic in the database, if one does not exist. Returns a topic object
+	#
+	##########
 	def getTopicFromName(self, topicName):
 		global conn
 		global cur
@@ -76,10 +91,16 @@ class Scraper:
 
 		#print("Creating topic with id "+str(topicId)+" and name "+str(topicName))
 		#topic = Topic(topicId, topicName)
+
+		#If not using MySQL, return a dummy topic object. This line should be commented
+		#out if you are using MySQL
 		topic = Topic(0, topicName, "")
 		return topic
 
-
+	################
+	# Utilty function used to get a Beautiful Soup object
+	# from a given URL
+	##############
 	def getPage(self, url):
 		session = requests.Session()
 		headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"}
@@ -87,12 +108,23 @@ class Scraper:
 		bsObj = BeautifulSoup(req.text)
 		return bsObj
 
+
+   	################
+	# Utilty function used to get a string from a Beautiful Soup
+	# object and a selector. Returns an empty string if no object
+	# is found for the given selector
+	##############
 	def safeGet(self, pageObj, selector):
 		childObj = pageObj.select(selector)
 		if childObj is not None and len(childObj) > 0:
 			return childObj[0].get_text()
 		return ""
 
+
+	################
+	# Searches a given website for a given topic and records all 
+	# pages found
+	##############
 	def search(self, topic, site):
 		print(site.searchUrl+topic.name)
 		bsObj = self.getPage(site.searchUrl+topic.name)
@@ -111,6 +143,9 @@ class Scraper:
 			if title != "" and body != "":
 				self.storeContent(topic, title, body, url)
 
+	################
+	# Starts a search of a given website for a given topic
+	##############
 	def scrape(self, topicStr, targetSite):
 		global conn
 		global cur
@@ -119,6 +154,11 @@ class Scraper:
 		topic = self.getTopicFromName(topicStr)
 		self.search(topic, targetSite)
 
+#####################################################
+##### "User" code, outside the scraper class ########
+#####################################################
+
+#Get topics.txt, throw an error if it is not provided
 if len(sys.argv) < 2:
 	sys.exit("Must provide a filename to read from")
 
@@ -127,12 +167,16 @@ topicName = f.readline().strip()
 scraper = Scraper()
 scraper.openCon()
 
+#Get a list of sites to search from the sites.csv file
 data = open('sites.csv', 'r').read()
-#data = urlopen("pages.csv")
 dataFile = StringIO(data)
 siteRows = csv.reader(dataFile)
-#Skip the heder line in the CSV file
+
+#Skip the heder line in the CSV file - the header makes it easy to read,
+#but we don't want to use the column titles as actual site data
 next(siteRows)
+
+#build a list of websites to search, from the CSV file
 sites = []
 for row in siteRows:
 	sites.append(Website(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
